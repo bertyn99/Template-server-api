512535bf7c1585969cd96c3a00f0ea90379b3267
bertyn99 <bboulikou@gmail.com>
2021-01-30 22:42:11 +0100

    ajout route user

---
+++ routes/user.js
@@ -0,0 +1,40 @@
+// To declare
+//db and schema
+const User = require("../db/type/user");
+const UserShema = require("../db/shema/user");
+const database = require("../db/connection");
+
+const bcrypt = require("bcrypt");
+
+const isUserExist = require("../services/isUserExist");
+
+// Function
+async function registerUser(req, res) {
+    if (!req.body.mail || !req.body.firstname || !req.body.lastname || !req.body.address || !req.body.city || !req.body.zip || !req.body.mobile || !req.body.password) {
+        return res.status(400).json({
+            status: "Veuillez remplir completement le formulaire d'inscription",
+        });
+    }
+
+    const salt = await bcrypt.genSalt();
+    const password = await bcrypt.hash(req.body.password, salt);
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    //const ip = req.headers["x-forwarded-for"].split(',')[0] || req.connection.remoteAddress;
+
+    if (await isUserExist.byMail(req.body.mail)) {
+        return res.status(400).json({
+            status: "Votre adresse mail est déjà utiliser",
+        });
+    }
+
+    newUser = new UserShema(new User(req.body.mail, req.body.firstname, req.body.lastname, req.body.address, req.body.city, req.body.zip, password, req.body.mobile, ip));
+
+    newUser.save((err, data) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(400);
+        } else {
+            return updateUser(data._id, randKey.generate(250), res);
+        }
+    });
+}
