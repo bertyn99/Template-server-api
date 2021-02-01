2228de0e725cfa0de71487ecb6862114fc9bca50
bertyn99 <bboulikou@gmail.com>
2021-02-01 12:33:17 +0100

    correction email et password userschema

--- db/schéma/user.js
+++ db/schéma/user.js
@@ -4,10 +4,46 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-    mail: { type: String, required: [true, 'Please enter an email'], lowercase: true },
+    mail: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid Email.')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain password.')
+            }
+            /* add number in password
+            if(value.includes()){
+                throw new Error('Password need to contain a number.')
+            } 
+            add one Maj a t least in the password
+            if(value.includes()){
+                throw new Error('Password need to contain a number.')
+            } 
+            
+            */
+        }
+    },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    password: { type: String, required: true },
     createDate: { type: Number, required: true },
     tokenSession: { type: String, required: false },
     createIp: { type: String, required: true },
