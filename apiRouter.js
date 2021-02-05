cb96b6d7a89a3479db5cc1ba235a9cb65e03f004
bertyn <bertynboulikou@gmail.com>
2021-02-05 14:10:03 +0100

    commentaire routeur et ajout du fichier emailing

--- apiRouter.js
+++ apiRouter.js
@@ -2,26 +2,31 @@ const express = require("express");
 const user = require("./routes/user");
 const verifyToken = require("./services/verifyToken");
 
-// register user
-apiRouter.route("/register").post(user.registerUser);
+exports.router = (function () {
+    let apiRouter = express.Router();
+    // register user
+    /* apiRouter.route("/register").post(user.registerUser);
+    
+    // connection user
+    apiRouter.route("/connect").post(user.connectUser);
+    
+    // reconnect user
+    apiRouter.route("/reconnect").post(verifyToken, lastView, user.reconnectUser);
+    
+    // my info
+    apiRouter.route("/info").get(verifyToken, user.myInfo);
+    
+    // info user
+    apiRouter.route("/info/:id").get(verifyToken, user.infoUser);
+    
+    // edit profile
+    apiRouter.route("/edit").put(verifyToken, user.editUser);
+    
+    // lost password - client
+    apiRouter.route("/lost").post(user.lostPassword);
+    
+    // lost password - website
+    apiRouter.route("/lost/reset").post(user.resetPassword); */
 
-// connection user
-apiRouter.route("/connect").post(user.connectUser);
-
-// reconnect user
-apiRouter.route("/reconnect").post(verifyToken, lastView, user.reconnectUser);
-
-// my info
-apiRouter.route("/info").get(verifyToken, user.myInfo);
-
-// info user
-apiRouter.route("/info/:id").get(verifyToken, user.infoUser);
-
-// edit profile
-apiRouter.route("/edit").put(verifyToken, user.editUser);
-
-// lost password - client
-apiRouter.route("/lost").post(user.lostPassword);
-
-// lost password - website
-apiRouter.route("/lost/reset").post(user.resetPassword);
+    return apiRouter;
+})();
