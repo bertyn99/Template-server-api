c81361b6365141e05bae2a0d3922f6639e2ebb62
bertyn <bertynboulikou@gmail.com>
2021-02-05 12:19:08 +0100

    new verify token

--- services/verifyToken.js
+++ services/verifyToken.js
@@ -1,14 +1,21 @@
-const UserShema = require("../db/shema/user");
+const UserShema = require("../db/schema/user");
+const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
-    const searchBySessionToken = await UserShema.findOne({ tokenSession: req.headers['tokensession'] });
-    if (searchBySessionToken) {
-        req.userByToken = searchBySessionToken;
-        next();
-    } else {
-        res.sendStatus(418);
-        return false;
+        if (!user) {
+            throw new Error()
+        }
+
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
     }
 
 }
