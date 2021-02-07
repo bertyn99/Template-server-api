140fc6ff2af341f1ff1c1ca943bae01dd892b0d3
bertyn99 <bboulikou@gmail.com>
2021-02-07 16:45:34 +0100

    correction des import et du de la variable d'environement

--- services/verifyToken.js
+++ services/verifyToken.js
@@ -1,12 +1,12 @@
-const UserShema = require("../db/schema/user");
+const User = require("../db/schema/user");
 const jwt = require('jsonwebtoken');
 
 const verifyToken = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_KEY)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
-
+        console.log(user)
         if (!user) {
             throw new Error()
         }
