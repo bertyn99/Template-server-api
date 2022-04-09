3d1612178ac138d34876f1fa68d416be4ddf584c
bertyn99 <bboulikou@gmail.com>
2022-04-09 15:54:47 +0200

    fix path and ad route

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 // Dev comments :
 
-const express = require('express')
+const express = require("express");
 const app = express();
-const compression = require('compression')
-const config = require('./config');
-/* const apiRouter = require("./apiRouter").router; */
+const compression = require("compression");
+const config = require("./config");
+const apiRouter = require("./apiRoute").router;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-
+app.use("/api", apiRouter);
 app.listen(config.PORT, () => {
-    console.log(`Application listening on port ${config.PORT}!`);
+  console.log(`Application listening on port ${config.PORT}!`);
 });
