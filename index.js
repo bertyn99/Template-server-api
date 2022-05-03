e32ab32b97178d064c37a623d2ffc7b51b5eeb9f
Bertyn Boulikou <bboulikou@gmail.com>
2022-05-03 18:26:56 +0200

    ajout log  de requete dans le terminal

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 const express = require("express");
 const app = express();
 const compression = require("compression");
+const morgan = require("morgan");
 const config = require("./config");
 const apiRouter = require("./route").router;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(compression());
+app.use(morgan("tiny"));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
