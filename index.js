dccf80a66866c6a4b2806d14862e3d9de619f748
bertyn99 <bboulikou@gmail.com>
2024-01-29 17:25:55 +0100

    Refactor logging in index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-// Dev comments :
-
 import express, { urlencoded, json } from "express";
-const app = express();
 import compression from "compression";
-import morgan from "morgan";
+import winston from "winston";
 import config from "./config.js";
 import { router as apiRouter } from "./route.js";
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
 const { PORT } = config;
+const app = express();
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(compression());
-app.use(morgan("tiny"));
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
